af7a8f7d7e1fe7a8dca7aed49979a643fa822de2
Bartosz Osowski <bartosz.osowski@errigal.com>
2020-03-01 22:36:32 +0000

    Refactor: Template list page; HTTP API calls centralized

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,42 +1,15 @@
 import React, { useState, useEffect } from "react";
-import Header from "../components/headerMovieList";
-import MovieList from "../components/movieList";
-import FilterControls from "../components/filterControls";
 import StubAPI from "../api/stubAPI";
-
+import PageTemplate from '../components/templateMovieListPage'
+import { getMovies } from "../api/tmdb-api";
 
 const MovieListPage = () => {
-  const [titleFilter, setTitleFilter] = useState("");
-  const [genreFilter, setGenreFilter] = useState("0");
   const [movies, setMovies] = useState([]);
-
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-      .then(res => res.json())
-      .then(json => {
-        console.log(json)
-        return json.results
-      })
-      .then(movies => {
-        setMovies(movies);
-      });
-  }, []);
-
-  const genre = Number(genreFilter);
-  let displayedMovies = movies
-    .filter(m => {
-      return m.title.toLowerCase().search(titleFilter.toLowerCase()) !== -1;
-    })
-    .filter(m => {
-      return genre > 0 ? m.genre_ids.includes(Number(genreFilter)) : true;
+    getMovies().then(movies => {
+      setMovies(movies);
     });
-
-  const handleFilterChange = (type, value) => {
-    if (type === "name") setTitleFilter(value);
-    else setGenreFilter(value);
-  };
+  }, []);
 
   const addToFavorites = movieId => {
     setMovies(movies => {
@@ -49,14 +22,11 @@ const MovieListPage = () => {
   };
 
   return (
-    <>
-      <Header numMovies={displayedMovies.length} />
-      <FilterControls onUserInput={handleFilterChange} />
-      <MovieList
-        movies={displayedMovies}
+      <PageTemplate
+        title='All Movies'
+        movies={movies}
         buttonHandler={addToFavorites}
       />
-    </>
   );
 };
 
