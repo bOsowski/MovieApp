2311ded549db6ff6b702dd8a7797a56dbee759ce
Bartosz Osowski <bartosz.osowski@errigal.com>
2020-03-11 01:55:07 +0000

    Refactor: Movie list and genre list now stored in contexts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import HomePage from "./pages/homePage.js";
-import MoviePage from './pages/moviePage.js'
-import FavoriteMoviesPage from './pages/favoritesMoviesPage.js'
-import MovieReviewPage from './pages/movieReviewPage.js'
-import SiteHeader from './components/siteHeader'
+import MoviePage from './pages/moviePage.js';
+import FavoriteMoviesPage from './pages/favoritesMoviesPage.js';
+import MovieReviewPage from './pages/movieReviewPage.js';
+import SiteHeader from './components/siteHeader';
+import MoviesContextProvider from "./contexts/moviesContext";
+import GenresContextProvider from "./contexts/genresContext"
 
 const App = () => {
   return (
@@ -14,13 +16,17 @@ const App = () => {
       <div className="jumbotron">
         <SiteHeader />      {/* New Header  */}
         <div className="container-fluid">
-          <Switch>
-            <Route path="/reviews/:id" component={MovieReviewPage} />
-            <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
-            <Route path="/movies/:id" component={MoviePage} />
-            <Route path="/" component={HomePage} />
-            <Redirect from="*" to="/" />
-          </Switch>
+          <MoviesContextProvider>
+            <GenresContextProvider>
+              <Switch>
+                <Route path="/reviews/:id" component={MovieReviewPage} />
+                <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+                <Route path="/movies/:id" component={MoviePage} />
+                <Route path="/" component={HomePage} />
+                <Redirect from="*" to="/" />
+              </Switch>
+            </GenresContextProvider>
+          </MoviesContextProvider>
         </div>
       </div>
     </BrowserRouter>
