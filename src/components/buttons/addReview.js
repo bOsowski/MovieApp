60447db6d8b1391fd2af0f075151a933d804929c
Bartosz Osowski <bOsowski04@gmail.com>
2020-03-22 03:43:44 +0000

    Add review supported

--- src/components/buttons/addReview.js
+++ src/components/buttons/addReview.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const AddReviewButton = () => {
-    return (
-        <button
-            type="button"
-            className="btn w-100 btn-primary"
-        >
-            Write a Review
-        </button>
-    )
-}
+const ReviewButton = ({ movie }) => {
+  return (
+    <Link
+      className="btn w-100 btn-primary "
+      to={{
+        pathname: `/reviews/form`,
+        state: {
+          movie: movie
+        }
+      }}
+    >
+      Write a Review
+    </Link>
+  );
+};
 
-export default AddReviewButton
+export default ReviewButton;
