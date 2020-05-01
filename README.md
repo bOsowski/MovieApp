a8fa8586bb36f6d72c84c045b74a68fd32257278
Bartosz Osowski <bosowski04@gmail.com>
2020-05-01 02:43:21 +0100

    Updated the readme as per requirements. Fixed the storybook to correctly render the cast and videos components.

--- README.md
+++ README.md
@@ -1,4 +1,60 @@
-### create ./.env with (API Key (v3 auth))
-   > REACT_APP_TMDB_KEY=https://www.themoviedb.org/settings/api
+# Assignment 1 - ReactJS app.
 
-tutorial at: https://tutors-design.netlify.com/lab/ewd-2020-wit.netlify.com/topic04/book-3/Startup
+Name: Bartosz Osowski
+
+## Overview.
+#### Additional objectives:
+* Addition of embedded youtube videos related to the movies which can be seen on the movie details page.
+* Addition of the movie cast for each movie. 
+
+
+## Setup requirements.
+#### Packages
+* nodejs (version used = 13.12.0)
+#### Setup
+* execute `npm install && npm start`
+
+## Data Model Design.
+
+#### Additional TMDB endpoints used and sample responses, in JSON:
+|endpoint|Sample Response|
+| -- | --
+|`/api/movies/419704/credits`|`{"id": 419704, "cast": [{"cast_id": 271,"character": "Roy McBride","credit_id": "5e93b22dd55e4d001a19f94a","gender": 2, "id": 287, "name": "Brad Pitt", "order": 1,"profile_path": "/tJiSUYst4ddIaz1zge2LqCtu9tw.jpg", ...}]}`
+|`/api/movies/419704/videos`|`{"id":419704,"results":[{"id":"5cf81bfb92514153b7b9e733","iso_639_1":"en","iso_3166_1":"US","key":"P6AaSMfXHbA","name":"Official Trailer #1","site":"YouTube","size":1080,"type":"Trailer"}, ...]}`
+
+## App Design.
+
+### Component catalogue.
+
+![Cast](./storybook_screenshot_with_cast.png)
+![Videos](./storybook_screenshot_with_videos.png)
+
+### Design patterns.
+
++ The container pattern was used for the cast. A "creditsCard" was embedded in a "credits" component.
++ Custom hooks have been used pretty much for every component that required an API call.
+
+## UI Design.
+
+#### Details page
+![DetailsPage](./movie_details_additions.png)
+>Shows the movie's cast and related youtube videos.
+
+## Routing.
+
+#### Private
++ `/movies`
++ `/movies/$id`
++ `/movies/favorites`
++ `/movies/$id/reviews`
++ `/movies/$id/cast`
++ `/reviews/form`
++ `/reviews/$id`
+
+#### Non-private
++ `/login`
++ `/register`
+
+## Independent learning.
+
++ Further research on hooks and nested components.
--- stories/index.js
+++ stories/index.js
@@ -99,31 +99,8 @@ const sample = {
   vote_count: 9692
 };
 
-const sampleCast = {
-  "id": 570670,
-  "cast": [
-    {
-      "cast_id": 7,
-      "character": "Cecilia Kass",
-      "credit_id": "5c7bb553c3a36841dc1b2bbc",
-      "gender": 1,
-      "id": 32798,
-      "name": "Elisabeth Moss",
-      "order": 0,
-      "profile_path": "/vZDe22EtLtcHvPr28l4ZM0b7qjG.jpg"
-    },
-    {
-      "cast_id": 11,
-      "character": "James Lanier",
-      "credit_id": "5d0e4ff40e0a2602a1cc1712",
-      "gender": 2,
-      "id": 83860,
-      "name": "Aldis Hodge",
-      "order": 2,
-      "profile_path": "/bwnP5uuAsJgceLF2kJbeMJN0ONP.jpg"
-    }
-  ]
-}
+const sampleCast = {"id":419704,"cast":[{"cast_id":271,"character":"Roy McBride","credit_id":"5e93b22dd55e4d001a19f94a","gender":2,"id":287,"name":"Brad Pitt","order":1,"profile_path":"/tJiSUYst4ddIaz1zge2LqCtu9tw.jpg"},{"cast_id":272,"character":"H. Clifford McBride","credit_id":"5e93b248875d1a001944ef1d","gender":2,"id":2176,"name":"Tommy Lee Jones","order":2,"profile_path":"/rdKKuq8zWCDYlKSxIfwGo2RUjpN.jpg"},{"cast_id":273,"character":"Helen Lantos","credit_id":"5e93b2afd55e4d001a19fa27","gender":1,"id":17018,"name":"Ruth Negga","order":3,"profile_path":"/aL4nmTVtQU2uQDNdBCarTy413CZ.jpg"},{"cast_id":274,"character":"Lieutenant General Rivas","credit_id":"5e93b30c54508d001a177e3a","gender":2,"id":40543,"name":"John Ortiz","order":4,"profile_path":"/o2SOFI53N6Gv5S3fDEBNbeimbzY.jpg"},{"cast_id":275,"character":"Eve","credit_id":"5e93b31f54508d001517734e","gender":1,"id":882,"name":"Liv Tyler","order":5,"profile_path":"/jrOj6t9pH8TqO65pnsRuvRNhwqx.jpg"},{"cast_id":276,"character":"Thomas Pruitt","credit_id":"5e93b33154508d001a177e6a","gender":2,"id":55636,"name":"Donald Sutherland","order":6,"profile_path":"/pvk7fjJNg9kZQTeuG6ZlfJ8t2Ze.jpg"},{"cast_id":277,"character":"Chip Garnes","credit_id":"5e93b52184448e001a90f074","gender":2,"id":231,"name":"Greg Bryk","order":7,"profile_path":"/1I3SxKFvQSam6KOMT4j5f0nFxRg.jpg"},{"cast_id":278,"character":"Donald Stanford","credit_id":"5e93b531cedac4001bebaead","gender":2,"id":11628,"name":"Loren Dean","order":8,"profile_path":"/zCYDwYJjrY4lpeueZYSzAXKLD5h.jpg"},{"cast_id":279,"character":"Lorraine Deavers","credit_id":"5e93b5428e87020014cd5d03","gender":1,"id":55314,"name":"Kimberly Elise","order":9,"profile_path":"/pAYWZdsWYrGc9lvMCivUU7STSWf.jpg"},{"cast_id":280,"character":"Brigadier General Stroud","credit_id":"5e93b570ccb15f00136fe748","gender":2,"id":9277,"name":"John Finn","order":10,"profile_path":"/caCHMsqfpfEcK1tUlkLWV9KkEbS.jpg"},{"cast_id":281,"character":"Adjutant General Vogel","credit_id":"5e93b58584448e001a90f131","gender":1,"id":64908,"name":"LisaGay Hamilton","order":11,"profile_path":"/bqfowff4gF0cGaQ1c7F89HAL3DH.jpg"},{"cast_id":282,"character":"Captain Lawrence Tanner","credit_id":"5e93b5da8e87020013cd3fb1","gender":2,"id":83223,"name":"Donnie Keshawarz","order":12,"profile_path":null},{"cast_id":283,"character":"Franklin Yoshida","credit_id":"5e93b5eb54508d001a17813f","gender":2,"id":1545016,"name":"Bobby Nish","order":13,"profile_path":"/fuJyCgr9iZwFT5fEXuf0s4noCRg.jpg"},{"cast_id":284,"character":"Willie Levant","credit_id":"5e93b601d55e4d00151a24ee","gender":2,"id":62093,"name":"Sean Blakemore","order":14,"profile_path":"/oHarc1jlzPHV3WEeJcWcyicw5yc.jpg"},{"cast_id":285,"character":"Captain Lu","credit_id":"5e93b6128e87020016ccf1fa","gender":1,"id":51754,"name":"Freda Foh Shen","order":15,"profile_path":null}]}
+const sampleVideos = {"id":419704,"results":[{"id":"5cf81bfb92514153b7b9e733","iso_639_1":"en","iso_3166_1":"US","key":"P6AaSMfXHbA","name":"Official Trailer #1","site":"YouTube","size":1080,"type":"Trailer"},{"id":"5d313d7c326c1900101eba51","iso_639_1":"en","iso_3166_1":"US","key":"nxi6rtBtBM0","name":"Official Trailer #2","site":"YouTube","size":2160,"type":"Trailer"},{"id":"5d894d8a79b3d4001f832e8d","iso_639_1":"en","iso_3166_1":"US","key":"t6g0dsQzfqY","name":"Official Trailer #3","site":"YouTube","size":1080,"type":"Trailer"},{"id":"5d894d21d9f4a6000e4dc169","iso_639_1":"en","iso_3166_1":"US","key":"stOVFXuyyWQ","name":"Moon Rover","site":"YouTube","size":1080,"type":"Clip"},{"id":"5d894d5179b3d4002782dd61","iso_639_1":"en","iso_3166_1":"US","key":"Nvb9cDDFHtk","name":"Lima Project","site":"YouTube","size":1080,"type":"Clip"},{"id":"5d894d5cd9f4a600204da4ea","iso_639_1":"en","iso_3166_1":"US","key":"ykC_wu6ffOU","name":"Antenna","site":"YouTube","size":1080,"type":"Clip"}]}
 
 storiesOf("Home Page/MovieCard", module)
   .addDecorator(story => (
@@ -209,16 +186,32 @@ storiesOf("Movie Details Page/MovieReviews", module)
     <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
   ))
   .add("default", () => {
-    return <CreditsList movie={sample} />
+    const credits = sampleCast;
+    return <div className="row movies">{credits.cast ? credits.cast.map(c => {
+      return <CreditsCard key={c.cast_id} cast={c} />
+    }) : ("Waiting for cast info")}</div>;
   });
-
 
   storiesOf("Movie Details Page/Videos", module)
   .addDecorator(story => (
     <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
   ))
   .add("default", () => {
-    return <Videos movie={sample} />
+    const videos = sampleVideos
+    return (
+      <div class="video-div">
+      {videos.results ? videos.results.filter(v =>  v.site === 'YouTube').map( video => {
+     return <iframe
+          width="550"
+          height="315"
+          src={`https://www.youtube.com/embed/${video.key}`}
+          frameborder="0"
+          allow="accelerometer;autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen="true">
+       </iframe>
+      }) : "Waiting for videos"}
+      </div>
+    );
   });
 
 storiesOf("Movie Details Page/MovieReview", module)
